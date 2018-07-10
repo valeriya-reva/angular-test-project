a95c3fee09f3fad490a309897d79f2405d4dea1f
valeriya-reva <reva.valeri@gmail.com>
2018-07-10 08:39:00 +0300

    ''

--- dominikTest/src/app/header/header.component.ts
+++ dominikTest/src/app/header/header.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss', '../../assets/scss/style.scss']
+  styleUrls: ['./header.component.scss', '../../assets/scss/style.scss'],
+  outputs: [menuClick]
 })
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  menuClick = new EventEmitter();
 
   isnavActive = false;
   
